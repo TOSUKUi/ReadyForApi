54ca850fb116b6e3b242aa046854b8a3967f0382
TOSUKUi <am.businessmail.exp@gmail.com>
2017-10-28 17:27:41 +0900

    fixed time sleep too long bug

--- readyforapi/html_parser.py
+++ readyforapi/html_parser.py
@@ -61,7 +61,6 @@ class ProjectPageParser(Parser):
         project_json = ""
         for match in props_matches:
             project_json = match.groups()[0].replace('&quot;', '"')
-        print(project_json)
         return json.loads(project_json)
 
     def __project_tab_parser(self):
