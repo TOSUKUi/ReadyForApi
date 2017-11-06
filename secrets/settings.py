f5e2cc2c4a53a39e86e5002f838e96ba3271850e
TOSUKUi <am.businessmail.exp@gmail.com>
2017-11-07 00:50:38 +0900

    ignored unneeded fileds

--- secrets/settings.py
+++
@@ -1,5 +0,0 @@
-class Settings():
-    access_token = "191781241382934|FhFbsuN-S6Hx9CVYXd67NvCMQnA"
-    readyfor_domain = "https://readyfor.jp"
-    project_domain = "projects"
-    user_domain = "users"
